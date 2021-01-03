71099197d6bfc00dda042b6a22e897d4d5c080b0
plone <plone@nuovavoceecologista.it>
2021-01-03 16:39:39 +0100

    chore: production config

--- components/plone/ecosystem.config.js
+++ components/plone/ecosystem.config.js
@@ -3,16 +3,16 @@ module.exports = {
     {
       script: "./bin/zeoserver",
       args: "fg",
-      name: "rawmaterial-plone-zeoserver",
-      cwd: "/opt/rawmaterial/rawmaterial-plone",
-      interpreter: "/opt/rawmaterial/rawmaterial-plone/bin/python",
+      name: "rawmaterial-zeoserver",
+      cwd: "/opt/rawmaterial/rawmaterial.buildout/components/plone",
+      interpreter: "/opt/rawmaterial/rawmaterial.buildout/components/plone/bin/python",
     },
     {
       script: "./bin/instance1",
       args: "console",
       name: "rawmaterial-plone-instance1",
-      cwd: "/opt/rawmaterial/rawmaterial-plone",
-      interpreter: "/opt/rawmaterial/rawmaterial-plone/bin/python",
+      cwd: "/opt/rawmaterial/rawmaterial.buildout/components/plone",
+      interpreter: "/opt/rawmaterial/rawmaterial.buildout/components/plone/bin/python",
     },
   ],
 };
